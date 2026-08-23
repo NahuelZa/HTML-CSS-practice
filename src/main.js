9d47a6fad89db9f2bce175d2b557a29622d54549
homedev <juanmartinpalazzolo@icloud.com>
2026-08-23 20:36:02 +0200

    - fix: this was relocated into app.js

--- src/main.js
+++ src/main.js
@@ -11,16 +11,3 @@ import './component/save-buttonComponent.js';
 import './component/searchBarComponent.js';
 // TODO: import components
 
-import { readLocaleStorage } from './utils/localeStorageUtils.js';
-import { LOCAL_STORAGE_KEYS } from './appConsts.js';
-
-const initStorageData = () => {
-  const rawData = readLocaleStorage(LOCAL_STORAGE_KEYS.COORDINATES) ?? [];
-  const savedCoordinates = new Map(rawData);
-
-  savedCoordinates.forEach((direccion) => {
-    app.state.coordinates.add(direccion);
-  });
-};
-
-initStorageData();
