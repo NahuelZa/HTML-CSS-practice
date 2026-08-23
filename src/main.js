c997525601ff426427f480cbb3b4565469a9288e
homedev <juanmartinpalazzolo@icloud.com>
2026-08-23 20:50:41 +0200

    - fix filename

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import './app.js';
 import './component/weatherComponent.js';
 import './component/tableComponent.js';
 import './component/mapComponent.js';
-import './component/save-buttonComponent.js';
+import './component/saveButtonComponent.js';
 import './component/searchBarComponent.js';
 
